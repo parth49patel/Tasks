c0427f900a973f9dab7785cad9c90a9beac48608
Parth Patel <4parth9@gmail.com>
2024-07-22 12:32:53 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Parth Patel on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
